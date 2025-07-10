625e780766989fef197e97a959bafc5ab4e9afe2
Monhabell <167910199+Monhabell@users.noreply.github.com>
2025-07-10 11:26:15 -0500

    update producto types

--- src/components/productos/types.ts
+++ src/components/productos/types.ts
@@ -1,18 +1,18 @@
-type Producto = {
-  id: string | number
-  name: string
-  price: number | string
-  compare_price?: number | string
-  stock?: number
-  images?: { id: number, product_id: number, url: string }[]  // Cambiado de img a images
-  created_at?: string | Date
-  qualification?: number
-  relevance?: number
-  slug?: string | null
-  description?: string
-  brand?: { id: number, name: string }
-  categories?: Array<{name: string, slug: string, description: string | null, image: string | null, parent_id: number | null}>
-}
+// type Producto = {
+//   id: string | number
+//   name: string
+//   price: number | string
+//   compare_price?: number | string
+//   stock?: number
+//   images?: { id: number, product_id: number, url: string }[]  // Cambiado de img a images
+//   created_at?: string | Date
+//   qualification?: number
+//   relevance?: number
+//   slug?: string | null
+//   description?: string
+//   brand?: { id: number, name: string }
+//   categories?: Array<{name: string, slug: string, description: string | null, image: string | null, parent_id: number | null}>
+// }
   
   // interface ListaProductosProps {
   //   productos: Producto[];
