5e4b4decd1329925871ccafa020beab222f18ff9
monhabell <monhabell@gmail.com>
2025-07-22 12:34:10 -0500

    [update permisos de imagen]

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-     domains: ['lh3.googleusercontent.com', '127.0.0.1', 'www.jcprola.com', 'img.freepik.com', 'www.softgenix.space'], // agrega aquí todos los dominios externos que uses
+    domains: [
+      'lh3.googleusercontent.com',
+      '127.0.0.1',
+      'www.jcprola.com',
+      'img.freepik.com',
+      'www.softgenix.space',
+      'softgenix.space', // ✅ Agrega este
+    ],
   },
 };
 
