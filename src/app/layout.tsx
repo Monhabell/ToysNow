89cafdf83adda6351da3ffa7b192e19f9d01a7d1
monhabell <monhabell@gmail.com>
2025-08-13 15:53:02 -0500

    [update] layaut para seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,42 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { CartProvider } from '@/context/CartContext'
 import SessionProviderWrapper from '@/components/SessionProviderWrapper'
+
 const inter = Inter({ subsets: ['latin'] })
+
 export const metadata = {
-  title: 'ToysNow',
-  description: 'Tienda erotica descubre una amplia selección de productos a precios inugualables. ¡déjate llevar por la imaginación y rompe con la monotonía!  ',
+  title: {
+    default: 'ToysNow - Tienda Erótica Premium',
+    template: '%s | ToysNow'
+  },
+  description: 'Descubre en ToysNow una amplia selección de productos eróticos de alta calidad. Juguetes sexuales, lencería y mucho más para explorar tu sexualidad. Envíos discretos y precios competitivos.',
+  keywords: ['juguetes eróticos', 'tienda sexual', 'productos para adultos', 'lencería sexy', 'vibradores', 'ToysNow'],
   icons: {
-    icon: '/images/logos/icono-logo-toys.ico', 
+    icon: '/images/logos/icono-logo-toys.ico',
+  },
+  openGraph: {
+    title: 'ToysNow - Tienda Erótica Premium',
+    description: 'Explora nuestra colección de productos eróticos diseñados para potenciar tu intimidad. Envío discreto y garantía de satisfacción.',
+    url: 'https://www.toysnow.com.co', // Cambiar por tu dominio real
+    siteName: 'ToysNow',
+    images: [
+      {
+        url: '/images/og-image.jpg', // Ruta a tu imagen para redes sociales
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'es_ES',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ToysNow - Tienda Erótica Premium',
+    description: 'Descubre productos que transformarán tu vida íntima. Envío discreto y garantía de calidad.',
+    images: ['/images/twitter-image.jpg'], // Ruta a tu imagen para Twitter
   },
 }
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
