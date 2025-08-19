e27aae2574188bbe73690d89c3914925da8e243c
monhabell <monhabell@gmail.com>
2025-08-19 17:33:01 -0500

    [update productos

--- src/app/productos/[id]/page.tsx
+++ src/app/productos/[id]/page.tsx
@@ -92,7 +92,7 @@ const SocialShare = ({
       {isOpen && (
         <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50 border border-gray-200">
           <div className="px-4 py-2 text-xs text-gray-500 border-b border-gray-100">
-            Compartir producto
+            Compartir Produstos
           </div>
           
           <button
